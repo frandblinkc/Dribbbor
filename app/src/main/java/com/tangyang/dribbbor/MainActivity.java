d58d7613cf1ba384935eef527f74111771408c89
Yang Tang <Yang Tang>
2016-09-15 20:39:44 -0500

    add basic navigation item selected listener

--- app/src/main/java/com/tangyang/dribbbor/MainActivity.java
+++ app/src/main/java/com/tangyang/dribbbor/MainActivity.java
@@ -1,16 +1,25 @@
 package com.tangyang.dribbbor;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.tangyang.dribbbor.view.shot_list.ShotListFragment;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.drawer) NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -22,5 +31,25 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.fragment_container, ShotListFragment.newInstance())
                     .commit();
         }
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.drawer_item_home:
+                        Toast.makeText(MainActivity.this, "home clicked", Toast.LENGTH_LONG);
+                        break;
+
+                    case R.id.drawer_item_likes:
+                        Toast.makeText(MainActivity.this, "likes clicked", Toast.LENGTH_LONG);
+                        break;
+
+                    case R.id.drawer_item_buckets:
+                        Toast.makeText(MainActivity.this, "buckets clicked", Toast.LENGTH_LONG);
+                        break;
+                }
+                return false;
+            }
+        });
     }
 }
