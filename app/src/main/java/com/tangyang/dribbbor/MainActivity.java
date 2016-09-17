75c2f0aaea28902f80393ece0989b9c9d6dbce03
Yang Tang <Yang Tang>
2016-09-17 14:09:33 -0500

    add navigation drawer with hamburger button control

--- app/src/main/java/com/tangyang/dribbbor/MainActivity.java
+++ app/src/main/java/com/tangyang/dribbbor/MainActivity.java
@@ -1,9 +1,12 @@
 package com.tangyang.dribbbor;
 
+import android.content.res.Configuration;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -18,6 +21,7 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.drawer) NavigationView navigationView;
     @BindView(R.id.drawer_layout) DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
         setUpDrawer();
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
+
 
         if (savedInstanceState == null) {
             getSupportFragmentManager()
@@ -41,6 +44,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setUpDrawer() {
+
+        drawerToggle = new ActionBarDrawerToggle(
+                                        this,
+                                        drawerLayout,
+                                        R.string.open_drawer,
+                                        R.string.close_drawer);
+
+        drawerLayout.addDrawerListener(drawerToggle);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -80,5 +94,29 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // sync the toggle state after onRestoreInstanceState has occured
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // handle action bar items
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
     }
 }
