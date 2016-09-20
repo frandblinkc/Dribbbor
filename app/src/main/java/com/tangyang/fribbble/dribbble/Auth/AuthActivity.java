a40dffabe152378c5ad2d7029e05162f9d81948f
Yang Tang <Yang Tang>
2016-09-20 00:03:19 -0500

    fix: toolbar stretching bug with set windowSoftInputMode in manifest; get authorization code

--- app/src/main/java/com/tangyang/fribbble/dribbble/Auth/AuthActivity.java
+++ app/src/main/java/com/tangyang/fribbble/dribbble/Auth/AuthActivity.java
@@ -1,11 +1,18 @@
 package com.tangyang.fribbble.dribbble.Auth;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.tangyang.fribbble.R;
 
@@ -17,17 +24,10 @@ import butterknife.ButterKnife;
  * Created by tangy on 9/19/2016.
  */
 public class AuthActivity extends AppCompatActivity {
-
-
-
-
-
-
-
-
-
     public static final String KEY_URL = "url";
     public static final String KEY_CODE = "code";
+
+    private String code;
 
 
     @BindView(R.id.progress_bar)ProgressBar progressBar;
@@ -44,9 +44,41 @@ public class AuthActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setTitle(R.string.activitiy_login_title);
 
+        progressBar.setMax(100);
+
         String url = Auth.REDIRECT_URI;
 
 
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.startsWith(Auth.REDIRECT_URI)) {
+                    Uri uri = Uri.parse(url);
+                    code = uri.getQueryParameter(KEY_CODE);
+                    Toast.makeText(AuthActivity.this, "code=" + code, Toast.LENGTH_LONG).show();
+
+                }
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                progressBar.setVisibility(View.VISIBLE);
+                progressBar.setProgress(0);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                progressBar.setProgress(newProgress);
+            }
+        });
 
 
 
