b0191f5fadc8c28c9f53bd888a50cf6e9d806f85
Your Name <you@example.com>
2016-09-27 23:01:30 -0500

    refactor: add abstract class SingleFragmentActivity, let ShotActivity extend it

---
+++ app/src/main/java/com/tangyang/fribbble/view/base/SingleFragmentActivity.java
@@ -0,0 +1,57 @@
+package com.tangyang.fribbble.view.base;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.tangyang.fribbble.R;
+import com.tangyang.fribbble.view.shot_detail.ShotFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by YangTang on 9/27/2016.
+ */
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    @BindView(R.id.toolbar) Toolbar toolbar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_fragment);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setTitle(getActivityTitle());
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.fragment_container, newFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @NonNull
+    protected abstract Fragment newFragment();
+
+    @NonNull
+    protected String getActivityTitle() {
+        return ""; // return empty activity title in base class as default title
+    }
+}
--- app/src/main/java/com/tangyang/fribbble/view/shot_detail/ShotActivity.java
+++ app/src/main/java/com/tangyang/fribbble/view/shot_detail/ShotActivity.java
@@ -1,59 +1,29 @@
 package com.tangyang.fribbble.view.shot_detail;
 
-import android.os.Bundle;
+
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 
-import com.tangyang.fribbble.R;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
+
+import com.tangyang.fribbble.view.base.SingleFragmentActivity;
+
 
 /**
  * Created by YangTang on 9/27/2016.
  */
-public class ShotActivity extends AppCompatActivity{
+public class ShotActivity extends SingleFragmentActivity{
     public static final String KEY_SHOT_TITLE = "shot title";
 
-    @BindView(R.id.toolbar) Toolbar toolbar;
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_single_fragment);
-        ButterKnife.bind(this);
-
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        setTitle("Shot detail");
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager()
-                        .beginTransaction()
-                        .add(R.id.fragment_container, newFragment())
-                        .commit();
-        }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     @NonNull
+    @Override
     protected Fragment newFragment() {
         return ShotFragment.newInstance(getIntent().getExtras());
     }
 
     @NonNull
+    @Override
     protected String getActivityTitle() {
         return getIntent().getStringExtra(KEY_SHOT_TITLE);
     }
